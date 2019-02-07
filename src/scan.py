a1548615d8adcef2e9cebafcd3c101f46a7e2297
Cheolwan-Park <cheolwan.park552@gmail.com>
2019-02-07 14:46:51 +0900

    create scan_as_rect function

--- src/scan.py
+++ src/scan.py
@@ -1,4 +1,4 @@
-from util import four_point_transform
+from util import scan_as_rect
 import numpy as np
 import argparse
 import cv2
@@ -8,19 +8,13 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to image")
 args = vars(ap.parse_args())
 
-# read image, calculate ratio
+# read image
 image = cv2.imread(args["image"])
-ratio = image.shape[0] / 500.0
-orig = image.copy()
-image = imutils.resize(image, height=500)
+warped = scan_as_rect(image)
 
-# convert to grayscale, find edges
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(gray, 75, 200)
-
-cv2.imshow("Image", image)
-cv2.imshow("Endged", edged)
+# show
+cv2.imshow("original", imutils.resize(image, height=650))
+cv2.imshow("scanned", imutils.resize(warped, height=650))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
--- src/util.py
+++ src/util.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import cv2
+import imutils
 
 def order_points(pts) :
     rect = np.zeros((4,2), dtype="float32")
@@ -52,3 +53,32 @@ def four_point_transform(image, pts):
 
     # return the warped image
     return warped
+
+def scan_as_rect(image) :
+    # calculate ratio
+    ratio = image.shape[0] / 500.0
+    orig = image.copy()
+    image = imutils.resize(image, height=500)
+
+    # convert to grayscale, find edges
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 75, 200)
+
+    # find contours
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+    screen_cnt = None
+    for c in cnts:
+        # approximate the contour
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+        # check contour has four points
+        if len(approx) == 4:
+            screen_cnt = approx
+            break
+
+    return four_point_transform(orig, screen_cnt.reshape(4, 2) * ratio)
