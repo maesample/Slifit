0942d0ac1e01135f162e742e0b7f69477cd7e0ca
bhbh1226 <bhbh1226@naver.com>
2019-02-16 00:58:21 +0900

    IdunnoHowToDo

--- web/frontend/src/containers/HomePage.js
+++ web/frontend/src/containers/HomePage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import './HomePage.css'
 
 class HomePage extends Component {
     render() {
         return (
             <div id="home-page">
-
+                <div className="mask">
+                    <h1 className="title"><b>s</b>lifit</h1>
+                </div>
             </div>
         )
     }
